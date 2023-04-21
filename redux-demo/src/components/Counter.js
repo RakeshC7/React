d94be38896d22445691a5a4c79efa49bbc95dcf7
Rakesh Chotaliya <rcdesign28@gmail.com>
2023-04-21 21:23:27 +0530

    redux toolkit, state slices, redux toolkit state ,multiple slices, reading & dispatch from new slice, and splited code in seperate file in store

--- redux-demo/src/components/Counter.js
+++ redux-demo/src/components/Counter.js
@@ -4,28 +4,33 @@
 // but we can also use in functional based component also. however, using useDispatch and useSelector hook is great.
 // import {connect} from 'react-redux';
 import { useSelector, useDispatch } from 'react-redux';
+import { counterActions } from './../store/counterSlice';
 import classes from './Counter.module.css';
 
 const Counter = () => {
   const dispatch = useDispatch();
-  const counter = useSelector( state => state.counter );
-  const showCounter = useSelector( state => state.showCounter );
+  const counter = useSelector( state => state.counter.counter );
+  const showCounter = useSelector( state => state.counter.showCounter );
 
   const incrementHandler = () => {
-    dispatch({ type: 'increment' });
+    // dispatch({ type: 'increment' });
+    dispatch(counterActions.increment());
   };
 
   const decrementHandler = () => {
-    dispatch({ type: 'decrement' });
+    // dispatch({ type: 'decrement' });
+    dispatch(counterActions.decrement());
   };
 
   // payload to action
   const increaseHandler = () => {
-    dispatch({ type: 'increase', amount: 10  })
+    // dispatch({ type: 'increase', amount: 10  });
+    dispatch(counterActions.increase(10)); // { type: SOME_UNIQUE_Identifier, payload: 10 }
   }
 
   const toggleCounterHandler = () => {
-    dispatch({ type: 'toggle' });
+    // dispatch({ type: 'toggle' });
+    dispatch(counterActions.toggleCounter());
   };
 
   return (
--- redux-demo/src/store/index.js
+++ redux-demo/src/store/index.js
@@ -1,39 +1,92 @@
-import { createStore } from 'redux';
+// import { createStore} from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
+import counterSliceReducer from './counterSlice';
+import authSliceReducer from './authSlice';
 
-const initialState = { counter: 0, showCounter : true }
+// const initialCounterState = { counter: 0, showCounter : true }
 
-const counterReducer = ( state = initialState, action ) => {
-    switch(action.type){
-        case 'increment' : 
-            return {
-                // we have to return all state because redux won't merge your existing state,
-                //  it takes what you return and replaces the existing state with it
-                counter: state.counter + 1,
-                showCounter: state.showCounter
-            };
+
+// 1st way of redux
+// const counterReducer = ( state = initialState, action ) => {
+//     switch(action.type){
+//         case 'increment' : 
+//             return {
+//                 // we have to return all state because redux won't merge your existing state,
+//                 //  it takes what you return and replaces the existing state with it
+//                 counter: state.counter + 1,
+//                 showCounter: state.showCounter
+//             };
         
-        case 'increase' : 
-            return {
-                counter: state.counter + action.amount,
-                showCounter: state.showCounter
-            };
+//         case 'increase' : 
+//             return {
+//                 counter: state.counter + action.amount,
+//                 showCounter: state.showCounter
+//             };
 
-        case 'decrement' : 
-            return {
-                counter: state.counter - 1,
-                showCounter: state.showCounter
-            };
+//         case 'decrement' : 
+//             return {
+//                 counter: state.counter - 1,
+//                 showCounter: state.showCounter
+//             };
 
-        case 'toggle' : 
-            return {
-                showCounter: !state.showCounter,
-                counter: state.counter
-            }
+//         case 'toggle' : 
+//             return {
+//                 showCounter: !state.showCounter,
+//                 counter: state.counter
+//             }
 
-        default : return state
+//         default : return state
+//     }
+// };
+
+// 2nd way of redux with reduxToolkit
+// const counterSlice = createSlice({
+//     name: 'counter',
+//     initialState : initialCounterState,
+//     reducers: {
+//         increment(state) {
+//             state.counter++;
+//         },
+//         decrement(state) {
+//             state.counter--;
+//         },
+//         increase(state, action) {
+//             state.counter += action.payload
+//         },
+//         toggleCounter(state){
+//             state.showCounter = !state.showCounter;
+//         }
+//     }
+// });
+
+// const initialAuthState = {
+//     isAuthenticated: false
+// }
+
+// const authSlice = createSlice({
+//     name: 'authentication',
+//     initialState: initialAuthState,
+//     reducers: {
+//         login(state) {
+//             state.isAuthenticated = true;
+//         },
+//         logout(state) {
+//             state.isAuthenticated = false;
+//         }
+//     }
+// });
+
+// const store = createStore(counterReducer);
+
+// NOTE: Here we can call only 1 configureStore wheather we have single slice or multiple
+// so, the solution is we can pass FUNCTION AS ARGUMENT but it'll also takes OBJECT
+const store = configureStore({
+    reducer: {
+        counter: counterSliceReducer,
+        auth: authSliceReducer
     }
-};
+});
 
-const store = createStore(counterReducer);
+// so, here individual reducers here will automatically be merged together into one main reducer,
 
 export default store;
